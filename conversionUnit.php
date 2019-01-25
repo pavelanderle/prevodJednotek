482a27147b76d97058e1c21c5adc9eab27ba7137
Pavel <anderle@spseplyen.cz>
2019-01-25 07:39:44 +0100

    Initial message

---
+++ conversionUnit.php
@@ -0,0 +1,8 @@
+<?php
+  function convertLength($value,$startUnit,$convUnit){
+    $ration = array('mm' => 0.001 , 'cm' => 0.01, 'm' => 1, 'km' => 1000);
+     // TODO: 
+
+    return $convValue;
+  }
+ ?>
