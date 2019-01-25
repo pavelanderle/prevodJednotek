18e1d09d2b6cfdc42b78f1c7d3bff363546ad5eb
Pavel <anderle@spseplyen.cz>
2019-01-25 11:03:40 +0100

    Výpočet

--- conversionUnit.php
+++ conversionUnit.php
@@ -1,7 +1,7 @@
 <?php
   function convertLength($value,$startUnit,$convUnit){
     $ration = array('mm' => 0.001 , 'cm' => 0.01, 'm' => 1, 'km' => 1000);
-     $convValue = $value
+     $convValue = $value*$ration["$convUnit"]/$ration["$startUnit"];
 
     return $convValue;
   }
