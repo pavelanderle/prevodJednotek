17910c84f4f5fa065bf106ab03323578bed8a8ee
Pavel <anderle@spseplyen.cz>
2019-01-29 09:44:15 +0100

    Odtranění řešení. Pro studenty

--- conversionUnit.php
+++ conversionUnit.php
@@ -1,9 +1,6 @@
 <?php
   function convertLength($value,$startUnit,$convUnit){
     $ration = array('mm' => 1000 , 'cm' => 10, 'm' => 1, 'km' => 0.001);
-     $convValue = $value*$ration["$startUnit"]/$ration["$convUnit"];
-
-    return $convValue;
-    // takhle je to správně
+     
   }
  ?>
