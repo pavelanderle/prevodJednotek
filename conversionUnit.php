195831267d45a19512f00d5b5032315eb281df60
pavelanderle <anderle@spseplzen.cz>
2019-01-29 10:06:52 +0100

    Oprava

--- conversionUnit.php
+++ conversionUnit.php
@@ -1,6 +1,7 @@
 <?php
   function convertLength($value,$startUnit,$convUnit){
     $ration = array('mm' => 1000 , 'cm' => 10, 'm' => 1, 'km' => 0.001);
-     
+     //todo;
+    return $valueConv;
   }
  ?>
